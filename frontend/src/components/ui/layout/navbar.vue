ac6a436a0ac7799b8dfe21ad8b715234caf2ca75
ItsIvanG <itsivangbiz@gmail.com>
2024-12-09 20:41:43 +0800

    dark mode toggle

--- frontend/src/components/ui/layout/navbar.vue
+++ frontend/src/components/ui/layout/navbar.vue
@@ -55,7 +55,27 @@
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-
+<!--        Dark/Light mode Toggle-->
+      <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="outline">
+              <Icon icon="radix-icons:moon" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Icon icon="radix-icons:sun" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span class="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem @click="mode = 'light'">
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem @click="mode = 'dark'">
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem @click="mode = 'auto'">
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <!-- User Account Dropdown -->
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -98,7 +118,11 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { Bell, LayoutGrid, Menu, Search, ShoppingCart, Users, Inbox, Settings, CreditCard, HelpCircle } from 'lucide-vue-next'
+import { Icon } from '@iconify/vue'
+import { useColorMode } from '@vueuse/core'
 
+// Pass { disableTransition: false } to enable transitions
+const mode = useColorMode()
 const router = useRouter()
 const authStore = useAuthStore()
 
