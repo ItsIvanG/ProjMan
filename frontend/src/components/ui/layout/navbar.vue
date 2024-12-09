10e31652136f72da931232a1fffa1022010a3c89
ItsIvanG <itsivangbiz@gmail.com>
2024-12-09 20:43:13 +0800

    dark mode toggle 2

--- frontend/src/components/ui/layout/navbar.vue
+++ frontend/src/components/ui/layout/navbar.vue
@@ -22,7 +22,27 @@
             />
           </div>
         </form>
-
+<!--        Dark/Light mode Toggle-->
+      <DropdownMenu>
+          <DropdownMenuTrigger as-child>
+            <Button variant="outline">
+              <Icon icon="radix-icons:sun" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Icon icon="radix-icons:moon" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span class="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem @click="mode = 'light'">
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem @click="mode = 'dark'">
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem @click="mode = 'auto'">
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <!-- Notifications Dropdown -->
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
@@ -55,27 +75,7 @@
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-<!--        Dark/Light mode Toggle-->
-      <DropdownMenu>
-          <DropdownMenuTrigger as-child>
-            <Button variant="outline">
-              <Icon icon="radix-icons:moon" class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Icon icon="radix-icons:sun" class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-              <span class="sr-only">Toggle theme</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem @click="mode = 'light'">
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem @click="mode = 'dark'">
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem @click="mode = 'auto'">
-              System
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+
         <!-- User Account Dropdown -->
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
